e360d4198b9c7b74f6df0122ac116f721c85e725
mukkymon <134375929+mukkymon@users.noreply.github.com>
2023-12-18 14:08:48 +0700

    Update index.php

---
+++ index.php
@@ -0,0 +1,21 @@
+<!doctype html>
+<html lang="en" data-mdb-theme="dark">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>MDBootstrap demo</title>
+    <!-- Font Awesome -->
+    <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0/css/all.min.css" rel="stylesheet" />
+    <!-- Google Fonts -->
+    <link href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700&display=swap"
+      rel="stylesheet" />
+    <!-- MDB -->
+    <link href="https://cdnjs.cloudflare.com/ajax/libs/mdb-ui-kit/7.0.0/mdb.min.css" rel="stylesheet" />
+  </head>
+  <body>
+    <h1>Hello, world!</h1>
+    <!-- MDB -->
+    <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/mdb-ui-kit/7.0.0/mdb.min.js">
+    </script>
+  </body>
+</html>
